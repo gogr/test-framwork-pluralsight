b9bd1f2a4b3bfdb417516e1fc0d5e9f2c9a15003
Yaroslav Danylchenko <danilchenkoy@gmail.com>
2020-05-13 19:22:22 -0400

    first commit

---
+++ configuration_reader/src/main/java/com/pluralsight/configuration/reader/Config.java
@@ -0,0 +1,24 @@
+package com.pluralsight.configuration.reader;
+
+import com.pluralsight.configuration.reader.properties.PropertiesLoader;
+import com.pluralsight.configuration.reader.properties.PropertyFile;
+
+/**
+ * Class for base tests properties - urls for test, browser name and version
+ */
+@PropertyFile("pathToConfig")
+public class Config {
+
+    private static Config config;
+
+    public static Config getConfig() {
+        if (config == null) {
+            config = new Config();
+        }
+        return config;
+    }
+
+    public Config() {
+        PropertiesLoader.populate(this);
+    }
+}
